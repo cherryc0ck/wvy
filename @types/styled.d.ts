cc9e9c29a84377885ff83265d7211fe123352118
cherryc0ck <qstar9537@naver.com>
2023-09-05 21:49:10 +0900

    :closed_lock_with_key: svgr 추가 (#1)

--- @types/styled.d.ts
+++ @types/styled.d.ts
@@ -5,8 +5,10 @@ import { theme } from "@/styles";
 type CustomTheme = typeof theme;
 
 declare module "styled-components" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface DefaultTheme extends CustomTheme {}
 }
+
 declare module "react" {
   interface Attributes {
     css?: CSSProp | CSSObject;
