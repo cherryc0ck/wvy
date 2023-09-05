720220ac1b6b35c2cd7c4657d9169f2ef9ec78c2
cherryc0ck <qstar9537@naver.com>
2023-09-05 21:49:28 +0900

    :bento: 로고 추가 (#1)

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,1 +1,3 @@
-export { ReactComponent as closeIcon } from "./close.svg";
+export { default as CloseIcon } from "@/assets/icons/close.svg";
+export { default as HamburgerIcon } from "@/assets/icons/hamburger.svg";
+export { default as LogoIcon } from "@/assets/icons/logo.svg";
