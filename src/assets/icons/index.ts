3dc712a7fee262b1e9d287d89cf26fc3df32fcca
cherryc0ck <qstar9537@naver.com>
2023-09-07 18:22:21 +0900

    :bento: 하트 아이콘 추가 (#1)

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as CloseIcon } from "@/assets/icons/close.svg";
 export { default as HamburgerIcon } from "@/assets/icons/hamburger.svg";
 export { default as LogoIcon } from "@/assets/icons/logo.svg";
+export { default as HeartIcon } from "@/assets/icons/heart.svg";
