2ca8ae5628c1390efec74f7819c14f437b14bc8e
cherryc0ck <qstar9537@naver.com>
2023-09-11 19:31:05 +0900

    :sparkles: 공통 타입 추가 (#1)

---
+++ src/types/post.ts
@@ -0,0 +1,9 @@
+export type PostMenuType = "전체" | "홈" | "뷰티" | "레저";
+export type PostContent = {
+  mainImg: string;
+  subImg: string[];
+  theme: string;
+  title: string;
+  content: string;
+  tags: string[];
+};
