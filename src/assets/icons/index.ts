704390b6914812de57931efb16fd63addb95d426
cherryc0ck <qstar9537@naver.com>
2023-09-12 19:47:24 +0900

    :bento: 드롭다운 화살표 아이콘 추가(#1)

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,3 +2,4 @@ export { default as CloseIcon } from "@/assets/icons/close.svg";
 export { default as HamburgerIcon } from "@/assets/icons/hamburger.svg";
 export { default as LogoIcon } from "@/assets/icons/logo.svg";
 export { default as HeartIcon } from "@/assets/icons/heart.svg";
+export { default as ChevronDownIcon } from "@/assets/icons/chevron-down.svg";
