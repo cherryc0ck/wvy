24af1de03ae7aa07901b9609bc867cd6f0f1214d
cherryc0ck <qstar9537@naver.com>
2023-09-13 00:44:20 +0900

    :bento: chevron 아이콘 추가 (#4)

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,3 +3,5 @@ export { default as HamburgerIcon } from "@/assets/icons/hamburger.svg";
 export { default as LogoIcon } from "@/assets/icons/logo.svg";
 export { default as HeartIcon } from "@/assets/icons/heart.svg";
 export { default as ChevronDownIcon } from "@/assets/icons/chevron-down.svg";
+export { default as ChevronLeftIcon } from "@/assets/icons/chevron-left.svg";
+export { default as ChevronRightIcon } from "@/assets/icons/chevron-right.svg";
