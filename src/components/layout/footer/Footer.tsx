153a93f5b622b186daf7d364e57fc7877bd6e6c5
cherryc0ck <qstar9537@naver.com>
2023-09-12 19:47:35 +0900

    :sparkles: Footer 구현 (#1)

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,11 +1,44 @@
 "use client";
 
+import Link from "next/link";
+import { useState } from "react";
+
+import { ChevronDownIcon } from "@/assets/icons";
 import * as S from "./styled";
 
 export default function Footer() {
+  const [isOpenContact, setIsOpenContact] = useState(false);
+
+  const handleDropdownContact = () => {
+    setIsOpenContact(!isOpenContact);
+  };
+
   return (
     <S.Footer>
-      <div>sss</div>
+      <S.List>
+        <li>
+          <Link href="">About</Link>
+        </li>
+        <li>
+          <Link href="">Help</Link>
+        </li>
+        <li>
+          <button onClick={handleDropdownContact}>
+            <span>Contact</span>
+            <ChevronDownIcon />
+          </button>
+        </li>
+      </S.List>
+      {isOpenContact && (
+        <S.ContactList>
+          <li>
+            <Link href="">Instagram</Link>
+          </li>
+          <li>
+            <Link href="">Facebook</Link>
+          </li>
+        </S.ContactList>
+      )}
     </S.Footer>
   );
 }
