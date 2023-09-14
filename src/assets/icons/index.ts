4e3520ed617e38a8819a82b0dfda49ba11df93aa
cherryc0ck <qstar9537@naver.com>
2023-09-14 13:11:25 +0900

    :bento: Search 아이콘 추가 (#4)

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -5,3 +5,4 @@ export { default as HeartIcon } from "@/assets/icons/heart.svg";
 export { default as ChevronDownIcon } from "@/assets/icons/chevron-down.svg";
 export { default as ChevronLeftIcon } from "@/assets/icons/chevron-left.svg";
 export { default as ChevronRightIcon } from "@/assets/icons/chevron-right.svg";
+export { default as SearchIcon } from "@/assets/icons/Search.svg";
