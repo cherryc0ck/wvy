cc9f36506740b949d081b5a9ba8ded042a64a85f
cherryc0ck <qstar9537@naver.com>
2023-09-15 22:25:59 +0900

    :lipstick: input default x 아이콘 수정 (#4)

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -168,6 +168,19 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     outline: 0;
   }
+  input::-ms-clear,
+  input::-ms-reveal {
+	  display:none;
+    width:0;
+    height:0;
+  }
+
+  input::-webkit-search-decoration,
+  input::-webkit-search-cancel-button,
+  input::-webkit-search-results-button,
+  input::-webkit-search-results-decoration {
+	  display:none;
+  }
   textarea {
     outline: none;
     resize: none;
@@ -209,6 +222,7 @@ const GlobalStyle = createGlobalStyle`
   pre {
     color: ${theme.colors.mainBlack};
   }
+
 `;
 
 export default GlobalStyle;
