38ad6a670c6d8e9136bcdb3296981d88f326eda3
cherryc0ck <qstar9537@naver.com>
2023-09-18 23:23:36 +0900

    :sparkles: 브랜드 버튼 컴포넌트 구현 (#5)

---
+++ src/components/common/button/brand/BrandButton.tsx
@@ -0,0 +1,34 @@
+import type { ThemeMode } from "@/types/common";
+import * as S from "./styled";
+import Image from "next/image";
+
+type BrandButtonProps = {
+  isSelected: boolean;
+  imgSrc: string;
+  mode: ThemeMode;
+  label: string;
+  name: string;
+  onSelectBrand: (brand: string) => void;
+};
+
+export default function BrandButton({
+  isSelected,
+  imgSrc,
+  mode,
+  label,
+  name,
+  onSelectBrand,
+}: BrandButtonProps) {
+  return (
+    <S.Button
+      mode={mode}
+      aria-selected={isSelected ? "true" : "false"}
+      onClick={() => onSelectBrand(name)}
+    >
+      <S.Figure>
+        <Image src={imgSrc} alt={label} fill priority />
+      </S.Figure>
+      <span>{label}</span>
+    </S.Button>
+  );
+}
