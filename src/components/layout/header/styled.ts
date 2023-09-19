1df1dfc15e5ef6102b956df55fb31f4b6b5f463d
cherryc0ck <qstar9537@naver.com>
2023-09-20 00:34:20 +0900

    :sparkles: 반응형 헤더 구현 (#9)

--- src/components/layout/header/styled.ts
+++ src/components/layout/header/styled.ts
@@ -5,6 +5,7 @@ export const Header = styled.header`
     ${theme.flex.row("space-between", "center")};
     width: 100%;
     padding: 16px 20px;
+
     background-color: ${theme.colors.white};
 
     h1 {
@@ -13,6 +14,68 @@ export const Header = styled.header`
       svg {
         width: 100%;
         height: 100%;
+      }
+    }
+
+    ${theme.media.tablet} {
+      padding: 32px 24px;
+      h1 {
+        width: 45px;
+        height: 60px;
+      }
+    }
+  `};
+`;
+
+export const MobileNav = styled.nav`
+  ${({ theme }) => css`
+    display: block;
+    position: absolute;
+    top: 64px;
+    left: 0;
+    z-index: ${theme.zIndex.modal};
+    width: 100%;
+    background-color: ${theme.colors.white};
+
+    ul {
+      ${theme.flex.col()};
+      width: 100%;
+    }
+
+    ul li {
+      width: 100%;
+      border-bottom: 1px solid ${theme.colors.gray300};
+    }
+
+    ul li a {
+      display: block;
+      width: 100%;
+      padding: 20px 0;
+      ${theme.typo.body.lg};
+      color: ${theme.colors.mainBlack};
+      text-align: center;
+    }
+
+    ${theme.media.tablet} {
+      display: none;
+    }
+  `};
+`;
+
+export const DesktopNav = styled.nav`
+  ${({ theme }) => css`
+    display: none;
+
+    ${theme.media.tablet} {
+      display: block;
+      ul {
+        ${theme.flex.row()};
+        gap: 111.5px;
+      }
+
+      ul li a {
+        ${theme.typo.body.md};
+        color: ${theme.colors.black};
       }
     }
   `};
@@ -25,6 +88,10 @@ export const MenuButton = styled.button`
       width: 32px;
       height: 32px;
       stroke: ${theme.colors.black};
+    }
+
+    ${theme.media.tablet} {
+      display: none;
     }
   `};
 `;
