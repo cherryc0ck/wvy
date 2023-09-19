3917feab35a08d7717a30c105d4f63754caf4a46
cherryc0ck <qstar9537@naver.com>
2023-09-20 00:34:04 +0900

    :adhesive_bandage: clean code (#9)

---
+++ src/styles/themes/foundations/media.ts
@@ -0,0 +1,8 @@
+export const media = {
+  mobile: `@media (min-width: 360px)`,
+  tablet: `@media (min-width:  768px)`,
+  desktop: `@media (min-width: 1024px)`,
+  images: {
+    sizes: `(min-width: 360px) 100vw`,
+  },
+};
